a79640aedaecbcdbbb808b5f5c40ee00d4d2f48b
Wocinco <marcotulioew@hotmail.com>
2016-02-19 10:23:53 -0600

    Added files via upload

---
+++ ola.mundo.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : ola.cpp
+// Author      : Marco
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hola_mundo!!!" << endl; // prints !!!Hola_mundo!!!
+	return 0;
+}
